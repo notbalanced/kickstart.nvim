e4e458acd91fee65d19693c057d8407d1044a364
Craig Moyer <craig.moyer@gmail.com>
2026-05-14 20:23:35 -0400

    Update guifont to be handled based on OS type (different name for windows). Add additional options from the chat-gpt-config.

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -5,6 +5,7 @@ vim.o.linebreak = true
 
 -- Indenting
 vim.o.smartindent = true
+vim.o.autoindent = true
 vim.o.autowrite = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
@@ -17,8 +18,32 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
-vim.o.guifont = 'CaskaydiaCove Nerd Font Mono:h12'
+
+-- Set guifont
+if vim.fn.has("win32") == 1 then
+    -- Windows specific font
+    vim.o.guifont = "CaskaydiaCove NF:h10"
+else
+    -- Linux font
+    vim.o.guifont = 'CaskaydiaCove Nerd Font Mono:h12'
+end
 
 
+-- Wrapping
+vim.opt.wrap = true         -- wrap lines by default
+vim.opt.breakindent = true
+vim.opt.linebreak = true
 
+-- Don't line wrap for following file types
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "cpp", "c", "h", "tpp", "cmake", "make", "py", "lua" },
+    callback = function()
+        vim.opt_local.wrap = false
+        vim.opt_local.linebreak = false
+        vim.opt_local.breakindent = false
+    end,
+})
 
+-- Whitespace chars
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", lead = "·", trail = "·", nbsp = "␣" }
